94412117207a1b406be734f855d383895c817d05
Julius Oeftiger <julius.oeftiger@students.unibe.ch>
2018-05-16 21:06:19 +0200

    .

--- src/kerra/system/Console.java
+++ src/kerra/system/Console.java
@@ -1,12 +1,13 @@
 package kerra.system;
 
+import java.io.IOException;
+
 public class Console {
 
     /**
      * TODO: Not working.
      */
-    public static void clear() {
+    public static void clear(int b) {
         System.out.print("\033[H\033[2J");
-        System.out.flush();
     }
 }
--- src/kerra/util/DirectedGraph.java
+++ src/kerra/util/DirectedGraph.java
@@ -1,4 +1,50 @@
 package kerra.util;
 
-public class Graph {
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class DirectedGraph<E> {
+
+    private ArrayList<Node> graph;
+
+    public DirectedGraph() {
+        this(10);
+    }
+
+    public DirectedGraph(int initialSize) {
+        this.graph = new ArrayList<>(initialSize);
+    }
+
+    public DirectedGraph(Node[] nodes) {
+        this(nodes.length);
+        Collections.addAll(graph, nodes);
+    }
+
+
+
+    public class Node {
+
+        private E element;
+        private Object[] conns;
+
+        public Node(@NotNull E element) {
+            this.element = element;
+        }
+
+        public void setConnections(@NotNull E[] targets, double[] weights) {
+            this.conns = new Object[targets.length];
+            for (int i = 0; i < targets.length; i++) conns[i] = new Connection(targets[i], weights[i]);
+        }
+    }
+
+    private class Connection {
+        private E target;
+        private double weight;
+        private Connection(@NotNull E target, double weight) {
+            this.target = target;
+            this.weight = weight;
+        }
+    }
 }
