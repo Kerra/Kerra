55c62e0df96915abbeb991586767dde30e604f0a
Julius Oeftiger <julius.oeftiger@students.unibe.ch>
2018-05-16 21:04:07 +0200

    began smth.

--- src/kerra/neural/network/FFNeuron.java
+++ src/kerra/neural/network/FFNeuron.java
@@ -4,13 +4,23 @@ import org.jetbrains.annotations.NotNull;
 
 public class FFNeuron extends ANeuron {
 
-
     public FFNeuron() {}
 
     public FFNeuron(@NotNull double[] weights) {
         super(weights);
     }
 
+    /**
+     * Orders the neuron to send its value along all connections.
+     *
+     * @param targets   the neurons of the next layer.
+     *                  must be of same length as the current weights
+     * @throws  ArrayIndexOutOfBoundsException if {@code targets.length != weights.length}
+     */
+    @Override
+    public void feedForward(@NotNull INeuron[] targets) {
+        for (int i=0; i<targets.length; i++) targets[i].input(getValue() * weights[i]);
+    }
 
     /**
      * Returns the value with applied activation function currently stored in the neuron.
@@ -23,19 +33,5 @@ public class FFNeuron extends ANeuron {
         this.value += bias;
         if (func != null) this.value = func.f(value);
         return value;
-    }
-
-
-
-    /**
-     * Orders the neuron to send its value along all connections.
-     *
-     * @param targets   the neurons of the next layer.
-     *                  must be of same length as the current weights
-     * @throws  ArrayIndexOutOfBoundsException if {@code targets.length != weights.length}
-     */
-    @Override
-    public void feedForward(@NotNull INeuron[] targets) {
-        for (int i=0; i<targets.length; i++) targets[i].input(getValue() * weights[i]);
     }
 }
--- src/kerra/neural/network/RecurrentNeuron.java
+++ src/kerra/neural/network/RecurrentNeuron.java
@@ -1,4 +1,28 @@
 package kerra.neural.network;
 
-public class RecurrentNeuron {
+import org.jetbrains.annotations.NotNull;
+
+public class RecurrentNeuron extends ANeuron {
+
+    /**
+     * Orders the neuron to send its value to the specified targets.
+     * Must be implemented by child class.
+     *
+     * @param targets the neurons of the next layer
+     */
+    @Override
+    public void feedForward(@NotNull INeuron[] targets) {
+
+    }
+
+    /**
+     * Returns the value with applied activation function currently stored in the neuron.
+     * Must be implemented by child class.
+     *
+     * @return the value currently stored
+     */
+    @Override
+    public double getValue() {
+        return 0;
+    }
 }
